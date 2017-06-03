5279c8fd55e86019ef064ba8c72e6366819ae395
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-06-03 19:03:05 +0200

    Add bottom navigatio

--- GoogleElements/App.js
+++ GoogleElements/App.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider, BottomNavigation } from 'react-native-material-ui';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 'save'
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ThemeProvider>
+        <BottomNavigation active={this.state.active} hidden={false} style={style} >
+          <BottomNavigation.Action
+            key="today"
+            icon="today"
+            label="Today"
+            onPress={() => this.setState({ active: 'today' })}
+          />
+          <BottomNavigation.Action
+            key="people"
+            icon="people"
+            label="People"
+            onPress={() => this.setState({ active: 'people' })}
+          />
+          <BottomNavigation.Action
+            key="save"
+            icon="save"
+            label="Save"
+            onPress={() => this.setState({ active: 'save' })}
+          />
+          <BottomNavigation.Action
+            key="settings"
+            icon="settings"
+            label="Settings"
+            onPress={() => this.setState({ active: 'settings' })}
+          />
+        </BottomNavigation>
+      </ThemeProvider>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: 'flex',
+    justifyContent: 'space-between',
+    backgroundColor: '#006064',
   },
 });
